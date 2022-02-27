1e6c893d0c1255272b4617ac3b49cb5ed034c93c
Rockwell Schrock <rockwell@schrock.me>
2022-02-26 19:47:50 -0500

    Improve Ping? Pong!

--- lib/lora_spike/radio.ex
+++ lib/lora_spike/radio.ex
@@ -7,8 +7,12 @@ defmodule LoraSpike.Radio do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  def ping_async(timeout \\ 1000) do
+    GenServer.cast(__MODULE__, {:start_ping_async, self(), timeout})
+  end
+
   def ping(timeout \\ 1000) do
-    GenServer.cast(__MODULE__, {:start_ping, self(), timeout})
+    GenServer.call(__MODULE__, {:start_ping_sync, timeout})
   end
 
   @impl GenServer
@@ -28,25 +32,42 @@ defmodule LoraSpike.Radio do
     {:ok,
      %{
        lora_pid: lora_pid,
-       pong_to_pid: nil
+       pongs_received: [],
+       ping_mode: nil,
+       ping_client: nil,
+       pinged_at: nil
      }}
   end
 
   @impl GenServer
-  def handle_info({:lora, %{packet: {:ping, %{from: from}}}}, state) do
-    packet = {:pong, %{from: this_device_id(), to: from}}
-    LoRa.send(state.lora_pid, packet)
+  def handle_info({:lora, %{packet: {:ping, payload}}}, state) do
+    pong_packet = {:pong, %{from: this_device_id(), to: payload.from}}
+
+    LoRa.send(state.lora_pid, pong_packet)
 
     {:noreply, state}
   end
 
-  def handle_info({:lora, %{packet: {:pong, %{to: to, from: from}} = packet}}, state) do
-    if to == this_device_id() && state.pong_to_pid do
-      Logger.debug("Pong received from #{from}")
-      send(state.pong_to_pid, packet)
-    end
+  def handle_info({:lora, %{packet: {:pong, payload}} = message}, state) do
+    if state.ping_mode != nil && payload.to == this_device_id() do
+      Logger.debug("Pong received from #{payload.from}")
 
-    {:noreply, state}
+      pong_info = %{
+        from: payload.from,
+        rtt: {System.monotonic_time(:millisecond) - state.pinged_at, :ms},
+        rssi: {message.rssi, :dBm},
+        snr: {message.snr, :dB},
+        time: message.time
+      }
+
+      if state.ping_mode == :async do
+        send(state.ping_client, {__MODULE__, :pong, pong_info})
+      end
+
+      {:noreply, %{state | pongs_received: [pong_info | state.pongs_received]}}
+    else
+      {:noreply, state}
+    end
   end
 
   def handle_info({:lora, info}, state) do
@@ -56,20 +77,60 @@ defmodule LoraSpike.Radio do
   end
 
   def handle_info(:stop_ping, state) do
-    {:noreply, %{state | pong_to_pid: nil}}
+    pongs = Enum.reverse(state.pongs_received)
+
+    case state.ping_mode do
+      :sync -> GenServer.reply(state.ping_client, pongs)
+      :async -> send(state.ping_client, {__MODULE__, :ping_complete})
+    end
+
+    {:noreply, %{state | ping_client: nil, pongs_received: [], ping_mode: nil, pinged_at: nil}}
   end
 
   @impl GenServer
-  def handle_cast({:start_ping, sender, ping_timeout}, state) do
-    packet = {:ping, %{from: this_device_id()}}
-    LoRa.send(state.lora_pid, packet)
-
-    if state.pong_to_pid do
+  def handle_cast({:start_ping_async, ping_client, ping_timeout}, state) do
+    if state.ping_client do
       {:noreply, state}
     else
+      transmit_ping(state)
+
       Process.send_after(self(), :stop_ping, ping_timeout)
-      {:noreply, %{state | pong_to_pid: sender}}
+
+      {:noreply,
+       %{
+         state
+         | ping_mode: :async,
+           ping_client: ping_client,
+           pinged_at: System.monotonic_time(:millisecond)
+       }}
     end
+  end
+
+  @impl GenServer
+  def handle_call({:start_ping_sync, ping_timeout}, from, state) do
+    if state.ping_client do
+      {:reply, {:error, :ping_in_progress}, state}
+    else
+      pinged_at = System.monotonic_time(:millisecond)
+
+      transmit_ping(state)
+
+      Process.send_after(self(), :stop_ping, ping_timeout)
+
+      {:noreply,
+       %{
+         state
+         | ping_mode: :sync,
+           ping_client: from,
+           pinged_at: pinged_at
+       }}
+    end
+  end
+
+  defp transmit_ping(state) do
+    ping_packet = {:ping, %{from: this_device_id()}}
+
+    LoRa.send(state.lora_pid, ping_packet)
   end
 
   defp this_device_id, do: Nerves.Runtime.serial_number()
